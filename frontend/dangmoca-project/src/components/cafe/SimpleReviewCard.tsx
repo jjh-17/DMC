4631f4add2f186dc9d62995f7cb33c3310ccc6d4
sujin <ansujin0204@naver.com>
2024-03-12 17:56:56 +0900

    [FE] Feat : 마이 리뷰 출력

--- frontend/dangmoca-project/src/components/cafe/SimpleReviewCard.tsx
+++ frontend/dangmoca-project/src/components/cafe/SimpleReviewCard.tsx
@@ -1,28 +1,32 @@
 interface Review {
-    reviewSeq: number;
-    memberSeq: number;
-    cafeSeq: number;
-    name: string;
-    image: string[];
-    content: string;
-    tag: string[];
-    rating: number;
-    createdDate: string;
-};
+  reviewSeq: number;
+  memberSeq: number;
+  cafeSeq: number;
+  name: string;
+  image: string[];
+  content: string;
+  tag: string[];
+  rating: number;
+  createdDate: string;
+}
 
 const SimpleReviewCard = (review: Review) => {
-    
-    return(
-        <>
-        <div>
-            {review.image.map((img, index) => (
-                <img key={index} src={img} alt={`Review ${review.reviewSeq} Image ${index}`} />
-            ))}
-        </div>
-        <h2>{review.name}</h2>
-        <p>{review.content}</p>
-        </>
-    );
+  return (
+    <>
+      <div>
+        {review.image.length > 0 &&
+          review.image.map((img, index) => (
+            <img
+              key={index}
+              src={img}
+              alt={`Review ${review.reviewSeq} Image ${index}`}
+            />
+          ))}
+      </div>
+      <h2>{review.name}</h2>
+      <p>{review.content}</p>
+    </>
+  );
 };
 
 export default SimpleReviewCard;
