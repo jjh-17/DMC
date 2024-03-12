3dec599fae51a21b39174493459fb68dd1e51147
sujin <ansujin0204@naver.com>
2024-03-12 11:24:17 +0900

    [FE] Fix : 버튼 타입스크립트 적용

--- frontend/dangmoca-project/src/components/common/Button.tsx
+++ frontend/dangmoca-project/src/components/common/Button.tsx
@@ -1,7 +1,17 @@
-export default function Button({text, handleClick }) {
-    return (
-      <button className="bg-white rounded-md shadow-md font-bold border-2 border-primary text-primary hover:bg-primary hover:text-white hover:border-white m-2 p-2" onClick={handleClick}>
-        {text}
-      </button>
-    );
-  }
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
+  text: string;
+  handleClick: () => void;
+}
+
+function Button (ButtonProps: ButtonProps): React.ReactElement {
+  return (
+    <button
+      className="bg-white rounded-md shadow-md font-bold border-2 border-primary text-primary hover:bg-primary hover:text-white hover:border-white m-2 p-2"
+      onClick={ButtonProps.handleClick}
+    >
+      {ButtonProps.text}
+    </button>
+  );
+};
+
+export default Button;
