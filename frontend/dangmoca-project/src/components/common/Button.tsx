1d64fcca2c9aefae4a80293e6b478dc5cc2d88eb
sujin <ansujin0204@naver.com>
2024-03-12 12:43:33 +0900

    Fix : 머지 수정

--- frontend/dangmoca-project/src/components/common/Button.tsx
+++ frontend/dangmoca-project/src/components/common/Button.tsx
@@ -6,11 +6,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
 function Button (ButtonProps: ButtonProps): React.ReactElement {
   return (
     <button
-<<<<<<< HEAD
-      className="bg-white rounded-md shadow-md font-bold border-2 border-primary text-primary hover:bg-primary hover:text-white hover:border-white m-2 p-2"
-=======
       className="bg-white rounded-md shadow-md font-bold border-2 border-primary text-primary hover:bg-primary hover:text-white hover:border-white m-2 p-2 w-fit"
->>>>>>> 96f04e14af2e0c01c642e11d9ef0e2f206ec69db
       onClick={ButtonProps.handleClick}
     >
       {ButtonProps.text}
