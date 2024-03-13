19dec303c4080d195b1c766fb3d693fc0518611a
sujin <ansujin0204@naver.com>
2024-03-13 09:49:24 +0900

    [FE] Fix : 머지 컨플릭트

--- frontend/dangmoca-project/src/router/routes.tsx
+++ frontend/dangmoca-project/src/router/routes.tsx
@@ -5,10 +5,7 @@ import MyPage from "../pages/mypage/MyPage";
 import CafeTest from "../pages/user/CafeTest";
 import ErrorPage from "../pages/ErrorPage";
 import CafeList from "../pages/cafe/CafeList";
-<<<<<<< HEAD
 import MyReview from "../pages/mypage/MyReview";
-=======
->>>>>>> cea3f51e823f556c11f2b53805d99559bb28cfaf
 
 const routes = createBrowserRouter([
   {
@@ -17,16 +14,10 @@ const routes = createBrowserRouter([
     children: [
       { path: "/", element: <Main /> }, // 메인화면 (첫 화면)
       { path: "login", element: <Login /> },
-<<<<<<< HEAD
       { path: "mypage", element: <MyPage /> },
       { path: "my-reviews", element: <MyReview /> },
       { path: "cafetest", element: <CafeTest /> },
       { path: "cafes", element: <CafeList />, children: [] },
-=======
-      { path: "members", element: <MyPage /> },
-      { path: "cafetest", element: <CafeTest /> },
-      { path: "cafes", element: <CafeList />, children:[] },
->>>>>>> cea3f51e823f556c11f2b53805d99559bb28cfaf
     ],
   },
   { path: "/*", element: <ErrorPage /> },
