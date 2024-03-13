77cc565e479cadca874dea0b92fa7dba8ad5dbad
sujin <ansujin0204@naver.com>
2024-03-13 17:49:23 +0900

    [FE] Feat : 푸터 경로이동

--- frontend/dangmoca-project/App.tsx
+++ frontend/dangmoca-project/App.tsx
@@ -1,15 +1,12 @@
-
 import { RouterProvider } from "react-router-dom";
 import routes from "./src/router/routes";
 
 function App() {
-
-
   return (
     <>
       <RouterProvider router={routes}></RouterProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/dangmoca-project/src/components/common/TheFooter.tsx
+++ frontend/dangmoca-project/src/components/common/TheFooter.tsx
@@ -2,13 +2,15 @@ import HomeSvg from "../../assets/icons/homepage.svg";
 import PinSvg from "../../assets/icons/pin.svg";
 import BookMarkSvg from "../../assets/icons/bookmark.svg";
 import ProfileSvg from "../../assets/icons/profile.svg";
+import {useNavigate} from "react-router-dom";
 
 export default function TheFooter() {
+    const navigate = useNavigate();
   const footerButtonStyle = "mx-4 font-bold py-2 px-4 rounded";
 
   return (
     <div className="fixed inset-x-0 bottom-0 bg-white border-t-primary border-t-2 text-center shadow-lg p-2">
-      <button className={footerButtonStyle}>
+      <button className={footerButtonStyle} onClick={() => navigate('/')}>
         <img src={HomeSvg} alt="홈" />
       </button>
       <button className={footerButtonStyle}>
@@ -17,7 +19,7 @@ export default function TheFooter() {
       <button className={footerButtonStyle}>
         <img src={BookMarkSvg} alt="북마크" />
       </button>
-      <button className={footerButtonStyle}>
+      <button className={footerButtonStyle} onClick={() => navigate('/mypage')}>
         <img src={ProfileSvg} alt="프로필" />
       </button>
     </div>
