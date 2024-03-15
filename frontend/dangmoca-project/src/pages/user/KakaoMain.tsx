1d9b61ee75b3162ba58830edef2fcdf1b1309ecc
dana <kdn1030@naver.com>
2024-03-15 15:43:20 +0900

    [FE] Feat : 사용자 토큰 받아오기

--- frontend/dangmoca-project/src/pages/user/KakaoMain.tsx
+++ frontend/dangmoca-project/src/pages/user/KakaoMain.tsx
@@ -11,7 +11,16 @@ export default function KakaoMain() {
 
   useEffect(() => {
     const code = queryParams.get("code");
-    axios.get("http://localhost:8080/account/kakao?code=" + code);
+    axios
+      .get("http://localhost:8080/account/kakao?code=" + code)
+      .then((response) => {
+        // TODO : 억세스 토큰을 쿠키에 저장하고, 리프레시 토큰 로칼스토레지에 담으세요.
+        // 담은 다음에, 메인 페이지로 유저 정보 가지고 이동 바랍니다.
+        const accessToken = response.headers.accesstoken;
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
   }, []);
 
   const handleClick = () => {
