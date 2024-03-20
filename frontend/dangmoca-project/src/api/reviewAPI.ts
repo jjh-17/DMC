dcb05cf92c3c74b249de98b3f24ff05f0a7266aa
sujin <ansujin0204@naver.com>
2024-03-20 14:12:23 +0900

    [FE] Feat : 리뷰 api 작성

--- frontend/dangmoca-project/src/api/reviewAPI.ts
+++ frontend/dangmoca-project/src/api/reviewAPI.ts
@@ -4,17 +4,34 @@ import { defaultAxios } from "./AuthCommon";
 const END_POINT = "reviews";
 
 interface ReviewData {
-    rating: number;
-    comment: string;
-    // 필요에 따라 더 많은 필드를 추가할 수 있습니다.
-  }
+  rating: number;
+  comment: string;
+  // 중요!@!@!@!@!@!!!
+  // 실제 데이터에 따라 수정하고 분리 필요!!!!!!
+  // 중요~~~~!!!!!
+}
 
 export const reviewAPI = {
+  // 리뷰 기본 CRUD
   writeReview(cafeId: string, reviewData: ReviewData): Promise<AxiosResponse> {
     return defaultAxios({
       method: "post",
       url: `${END_POINT}/cafe/${cafeId}`,
-      data: reviewData
+      data: reviewData,
+    });
+  },
+
+  getCafeReview(cafeId: string) {
+    return defaultAxios({
+      method: "get",
+      url: `${END_POINT}/cafe/${cafeId}`,
+    });
+  },
+
+  modifyReview(reviewId: string) {
+    return defaultAxios({
+      method: "patch",
+      url: `${END_POINT}/cafe/${reviewId}`,
     });
   },
 
@@ -22,6 +39,43 @@ export const reviewAPI = {
     return defaultAxios({
       method: "delete",
       url: `${END_POINT}/cafe/${reviewId}`,
+    });
+  },
+
+  // 사용자 리뷰 조회
+  getMyReview(memberId: string) {
+    return defaultAxios({
+      method: "get",
+      url: `${END_POINT}/member/${memberId}`,
+    });
+  },
+
+  // 좋아요 기능
+  likeReview(reviewId: string) {
+    return defaultAxios({
+      method: "post",
+      url: `${END_POINT}/cafe/like`,
+      params: {
+        reviewId: reviewId,
+      },
+    });
+  },
+
+  unlikeReview(reviewId: string) {
+    return defaultAxios({
+      method: "delete",
+      url: `${END_POINT}/cafe/like`,
+      params: {
+        reviewId: reviewId,
+      },
+    });
+  },
+
+  // 본인이 좋아요한 리뷰 조회
+  getLikeReview() {
+    return defaultAxios({
+      method: "get",
+      url: `${END_POINT}/member/like`,
     });
   },
 };
