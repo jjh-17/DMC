a847c1933c2a3bd57265e9f20f099cd84e3a5656
cyy8071 <cyy8071@gmail.com>
2024-03-21 15:46:56 +0900

    [FE] Feat : 카페 필터링 추가

---
+++ frontend/dangmoca-project/src/assets/data/tag.ts
@@ -0,0 +1,28 @@
+export const sort = [{ "거리순": "distance" }, { "영업중": "isOpen" }];
+
+export const tags = [
+    { "공부": "cagong" },
+    { "귀여운": "cute" },
+    { "데이트": "date" },
+    { "대형카페": "large" },
+    { "아기자기": "petit" },
+    { "조용한": "calm" },
+    { "SNS픽": "snspick" },
+    { "편안한": "cozy" },
+    { "커피": "coffee" },
+    { "디저트": "dessert" },
+    { "뷰": "view" },
+    { "분위기": "mood" },
+    { "야외": "outdoor" },
+    { "가성비": "reasonable" }
+];
+
+// 추후 수정가능
+export const desserts = [
+    { "마카롱": "macaron" },
+    { "케이크": "cake" },
+    { "브라우니" : "brownie"},
+    { "크레페" : "crape"},
+    { "치즈케이크" : "cheesecake" },
+
+];
--- frontend/dangmoca-project/src/pages/cafe/CafeList.tsx
+++ frontend/dangmoca-project/src/pages/cafe/CafeList.tsx
@@ -1,26 +1,164 @@
 import DetailCafeCard from "../../components/cafe/DetailCafeCard";
-import SimpleCafeCard from "../../components/cafe/SimpleCafeCard";
-import { useDragScroll } from "../../utils/useDragScroll";
 import cafeDummyData from "../../assets/testData/cafeDummyData";
+import SortIcon from '../../assets/icons/sort.svg?react';
+import DownArrowIcon from '../../assets/icons/downarrow.svg?react'
+import CafeFilterAndSort from "../../utils/CafeFilterAndSort";
+
+import { sort, tags, desserts } from '../../assets/data/tag'
+import { useState, useEffect, useRef } from "react";
+
+interface Cafe {
+  cafeSeq: number;
+  name: string;
+  distance: string;
+  address: string;
+  tag: string[];
+  isOpen: boolean;
+  dessertTag: string[];
+  imageUrl: string;
+}
 
 const CafeListPage = () => {
-  const [ref] = useDragScroll();
+  const [showFilter, setShowFilter] = useState(false);
+  const [showTagCheckbox, setShowTagCheckbox] = useState(false);
+  const [showDessertCheckbox, setShowDessertCheckbox] = useState(false);
+  const [cafeList, setCafeList] = useState<Cafe[]>([]);
+
+  const findKeyByValue = (object , value:string) => {
+    return Object.keys(object).find(key => object[key] === value);
+  };
+
+  useEffect(() => {
+    cafeDummyData.map(cafe => {
+      const updatedTags = cafe.tag.every((tag) => {
+        const tagName = findKeyByValue(tags, tag);
+        return tagName;
+      });
+
+      const updatedDessertTags = cafe.dessertTag.every(dessertTag => {
+        const tagName = findKeyByValue(desserts, dessertTag);
+        return tagName;
+      });
+
+      return {
+        ...cafe,
+        tag: updatedTags,
+        dessertTag: updatedDessertTags,
+      };
+    });
+    setCafeList(cafeDummyData);
+  }, [])
+
+  const selectedSorts = useRef<string[]>([]);
+  const selectedTags = useRef<string[]>([]);
+  const selectedDesserts = useRef<string[]>([]);
+
+  const handleSelectSort = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      selectedSorts.current.push(value);
+    } else {
+      selectedSorts.current = selectedSorts.current.filter(sort => sort !== value);
+    }
+  };
+
+  const handleSelectTags = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      selectedTags.current.push(value);
+    } else {
+      selectedTags.current = selectedTags.current.filter(sort => sort !== value);
+    }
+  };
+
+  const handleSelectDesserts = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      selectedDesserts.current.push(value);
+    } else {
+      selectedDesserts.current = selectedDesserts.current.filter(sort => sort !== value);
+    }
+  };
+
+  const submitFilter = () => {
+    setCafeList(cafeDummyData);
+    setCafeList(CafeFilterAndSort(cafeList, selectedSorts.current, selectedTags.current, selectedDesserts.current))
+  };
 
   return (
     <>
-      <div>
-        <div className="mx-auto w-[75lvw]">
-          <div ref={ref} className="mx-auto items-center flex flex-row overflow-x-scroll no-scroll">
-            {cafeDummyData.map((cafe) => (
-              <div key={cafe.cafeSeq}>
-                <SimpleCafeCard {...cafe} />
+      <div className="mt-[5lvh] mx-[20lvw]">
+        <div className="text-right">
+          <button className="relative right-0" onClick={() => setShowFilter(!showFilter)}>
+            정렬하기
+            <SortIcon className="w-4 h-4 inline-block mx-1" />
+          </button>
+          {showFilter && (
+            <div className="flex flex-col accent-primary3 bg-slate-50">
+              <div className="align-middle whitespace-pre-wrap">
+                {sort.map((item) => {
+                  const label = Object.keys(item);
+                  const value = Object.values(item);
+                  return (
+                    <span className="whitespace-nowrap ml-2" key={label[0]}>
+                      <label className="whitespace-nowrap">{label}</label>
+                      <input type="checkbox" value={value} onChange={handleSelectSort} />
+                    </span>);
+                })}
               </div>
-            ))}
-          </div>
+              <div>
+                태그 선택 체크박스 accordion <DownArrowIcon className="w-4 h-4 inline-block" onClick={() => setShowTagCheckbox(!showTagCheckbox)} />
+                {showTagCheckbox && (
+                  <div className="align-middle whitespace-pre-wrap">
+                    {tags.map((item) => {
+                      const label = Object.keys(item)[0];
+                      const value = Object.values(item)[0];
+                      return (
+                        <span className="whitespace-nowrap ml-2" key={label[0]}>
+                          <label className="whitespace-nowrap">#{label}</label>
+                          <input type="checkbox" value={value} onChange={handleSelectTags} />
+                        </span>);
+                    })}
+                  </div>
+                )}
+              </div>
+              <div>
+                디저트 선택 체크박스 accordion <DownArrowIcon className="w-4 h-4 inline-block" onClick={() => setShowDessertCheckbox(!showDessertCheckbox)} />
+                {showDessertCheckbox && (
+                  <div>
+                    {desserts.map((item) => {
+                      const label = Object.keys(item)[0];
+                      const value = Object.values(item)[0];
+                      return (
+                        <span className="whitespace-nowrap ml-2" key={label[0]}>
+                          <label className="whitespace-nowrap">{label}</label>
+                          <input type="checkbox" value={value} onChange={handleSelectDesserts} />
+                        </span>);
+                    })}
+                  </div>
+                )}
+              </div>
+              <div>
+                <button onClick={submitFilter}>GO FILTER</button>
+              </div>
+            </div>
+
+          )}
         </div>
         <div className="w-fit mx-auto">
           <div className="flex flex-col">
-            {cafeDummyData.map((cafe) => (
+            {cafeList.length == 0 && (
+              <div>
+                <p>해당하는 카페가 없습니다 TT ㅠㅠ 아이쿠</p>
+                <p>애니메이션 추가예정</p>
+              </div>
+            )}
+
+            {cafeList.length > 0 && cafeList.map((cafe) => (
+
+
+
+
               <div className="cursor-pointer" key={cafe.cafeSeq}>
                 <DetailCafeCard {...cafe} />
               </div>
