61064f1ffeadb6c80e868a2ecd88b30e40624aa8
sujin <ansujin0204@naver.com>
2024-03-21 09:38:44 +0900

    [FE] Feat : 좋아요 기능 초기

--- frontend/dangmoca-project/src/pages/review/CafeReview.tsx
+++ frontend/dangmoca-project/src/pages/review/CafeReview.tsx
@@ -1,11 +1,21 @@
+import { reviewAPI } from "../../api/reviewAPI";
 import DetailReviewCard from "../../components/review/DetailReviewCard";
 
 export default function CafeReview() {
-  
+  const handleLike = async () => {
+    try {
+      await reviewAPI.likeReview(4);
+      // 리뷰 아이디 로직 추가해야 함
+      // 좋아요 이미 누른 사람은 좋아요 취소 구현해야 함
+      console.log("좋아요 성공");
+    } catch (error) {
+      console.log("좋아요 실패");
+    };
+  };
 
   return (
     <>
-    <DetailReviewCard/>
+    <DetailReviewCard onLikeClick={handleLike}/>
     <p>데이터 들어오면 주석 처리한 리스트 로직으로 변경</p>
     </>
 
