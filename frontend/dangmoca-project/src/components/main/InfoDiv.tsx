7d711d74ea8aa00098bcad66a88f9afab77d80f9
cyy8071 <cyy8071@gmail.com>
2024-03-21 09:57:49 +0900

    [FE] Feat : 임시 메인화면

---
+++ frontend/dangmoca-project/src/components/main/InfoDiv.tsx
@@ -0,0 +1,22 @@
+interface infoProps {
+  imgUrl: string,
+  imgAlt: string,
+  title: string,
+  description: string
+}
+
+const InfoDiv = (props: infoProps) => {
+
+    return (
+    <div 
+    className="bg-[#BAA89B] flex flex-row justify-around bg-opacity-30 h-[30lvh] min-w-[40lvw] mt-[5lvh] items-center text-center shadow-md">
+        <img src={props.imgUrl} className="" alt={props.imgAlt}/>
+        <div>
+          <h1>{props.title}</h1>
+          <span>{props.description}</span>
+        </div>
+      </div>
+  )
+}
+
+export default InfoDiv
