e74289633bb4308a80425c9e36d23de154e190eb
김영진 <young285j@gmail.com>
2024-03-22 17:18:04 +0900

    Squashed commit of the following:
    commit 243adb0e46b8dbac1f7a91ebca4055f20adc0485
    Merge: 1354f90 1373b46
    Author: 안수진 <ansujin0204@naver.com>
    Date:   Fri Mar 22 16:14:19 2024 +0900
    
        Merge branch 'fe-feature/로그인' into 'fe-develop'
    
        Fe feature/로그인
    
        See merge request s10-bigdata-dist-sub2/S10P22A607!74
    
    commit 1373b4693129d9ef30ca1b5ace27740e30a9b54f
    Author: sujin <ansujin0204@naver.com>
    Date:   Fri Mar 22 16:13:15 2024 +0900
    
        [FE] Fix : 빌드 오류
    
    commit 56433d5b797fe28aa93641f433f69ec64e572741
    Author: sujin <ansujin0204@naver.com>
    Date:   Fri Mar 22 14:29:16 2024 +0900
    
        [FE] Fix : 빌드 오류

--- frontend/dangmoca-project/src/components/mypage/Profile.tsx
+++ frontend/dangmoca-project/src/components/mypage/Profile.tsx
@@ -49,7 +49,7 @@ const Profile = (user: User) => {
     },
   ]);
 
-  const options = {
+  const options:any = {
     colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"],
     enableTooltip: true,
     deterministic: false,
