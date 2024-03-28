c313cb2d506eb7103348fc29420fbb9c07874369
지준호 <jjho311@naver.com>
2024-03-28 15:28:45 +0900

    [BE] Docs : input 파일

--- hive/sql/scraping_data_table_init.sql
+++ hive/sql/scraping_data_table_init.sql
@@ -1,4 +1,5 @@
-DROP TABLE IF EXISTS kakao_cafe_info;
+use hive_db;
+
 DROP TABLE IF EXISTS kakao_cafe_menu;
 DROP TABLE IF EXISTS kakao_platform_review;
 DROP TABLE IF EXISTS naver_cafe_info;
@@ -6,21 +7,25 @@ DROP TABLE IF EXISTS naver_cafe_menu;
 DROP TABLE IF EXISTS naver_platform_review;
 
 -- 스크래핑한 카카오 카페 정보 테이블
+DROP TABLE IF EXISTS kakao_cafe_info;
 CREATE TABLE IF NOT EXISTS kakao_cafe_info  (
     cafe_name VARCHAR(100),
+    image_url VARCHAR(255),
+    rating FLOAT,
     latitude DOUBLE,
     longitude DOUBLE,
-    image_url VARCHAR(255),
     address VARCHAR(255),
     tel VARCHAR(25),
-    rating FLOAT,
     opening_hour VARCHAR(255),
     homepage_url VARCHAR(255)
 )
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY '\t'
 LINES TERMINATED BY '\n'
-STORED AS TEXTFILE;
+STORED AS TEXTFILE
+TBLPROPERTIES ('skip.header.line.count'='1');
+LOAD DATA INPATH '/hive/input/kakao_cafe.csv' INTO TABLE kakao_cafe_info;
+
 
 -- 스크래핑한 카카오 카페 메뉴 테이블
 CREATE TABLE IF NOT EXISTS kakao_cafe_menu  (
@@ -41,6 +46,50 @@ CREATE TABLE IF NOT EXISTS kakao_platform_review  (
     content STRING,
     nickname VARCHAR(50),
     platform STRING DEFAULT 'K',
+    created_date VARCHAR(255),
+    collected_date VARCHAR(255)
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+STORED AS TEXTFILE;
+
+-- 스크래핑한 네이버 카페 정보 테이블
+CREATE TABLE IF NOT EXISTS naver_cafe_info  (
+    cafe_name VARCHAR(100),
+    latitude DOUBLE,
+    longitude DOUBLE,
+    image_url VARCHAR(255),
+    address VARCHAR(255),
+    tel VARCHAR(25),
+    rating FLOAT,
+    opening_hour VARCHAR(255),
+    homepage_url VARCHAR(255)
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+STORED AS TEXTFILE;
+
+-- 스크래핑한 네이버 카페 메뉴 테이블
+CREATE TABLE IF NOT EXISTS naver_cafe_menu  (
+    cafe_name VARCHAR(100),
+    name VARCHAR(100),
+    price VARCHAR(50),
+    image_url VARCHAR(255),
+    dessert_tag VARCHAR(255)
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+STORED AS TEXTFILE;
+
+-- 스크래핑한 네이버 리뷰 테이블
+CREATE TABLE IF NOT EXISTS naver_platform_review  (
+    cafe_name VARCHAR(100),
+    content STRING,
+    nickname VARCHAR(50),
+    platform STRING DEFAULT 'N',
     created_date VARCHAR(255),
     collected_date VARCHAR(255)
 )
