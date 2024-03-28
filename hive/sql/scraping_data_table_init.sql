3944c3e12ff3692ca5edae7ade5105601599cf84
지준호 <jjho311@naver.com>
2024-03-28 12:15:33 +0900

    [BE] Feat : 카카오 카페 정보 저장 테이블 생성문

--- hive/sql/scraping_data_table_init.sql
+++ hive/sql/scraping_data_table_init.sql
@@ -5,10 +5,17 @@ DROP TABLE IF EXISTS naver_cafe_info;
 DROP TABLE IF EXISTS naver_platform_review;
 DROP TABLE IF EXISTS naver_cafe_menu;
 
+-- 스크래핑한 카카오 카페 정보 테이블
 CREATE TABLE IF NOT EXISTS kakao_cafe_info  (
-    column1 STRING,
-    column2 INT,
-    column3 DOUBLE
+    name VARCHAR(100),
+    latitude DOUBLE,
+    longitude DOUBLE,
+    image_url VARCHAR(255),
+    address VARCHAR(255),
+    tel VARCHAR(25),
+    rating FLOAT,
+    opening_hour VARCHAR(255),
+    homepage_url VARCHAR(255)
 )
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY '\t'
