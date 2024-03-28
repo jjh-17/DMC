40f2e6523b191fdece9d2b1c5e6a1a7415408b92
지준호 <jjho311@naver.com>
2024-03-28 12:29:22 +0900

    [BE] Feat : 카카오 카페 리뷰 저장 테이블 생성문 추가

--- hive/sql/scraping_data_table_init.sql
+++ hive/sql/scraping_data_table_init.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS kakao_cafe_info;
-DROP TABLE IF EXISTS kakao_platform_review;
 DROP TABLE IF EXISTS kakao_cafe_menu;
+DROP TABLE IF EXISTS kakao_platform_review;
 DROP TABLE IF EXISTS naver_cafe_info;
-DROP TABLE IF EXISTS naver_platform_review;
 DROP TABLE IF EXISTS naver_cafe_menu;
+DROP TABLE IF EXISTS naver_platform_review;
 
 -- 스크래핑한 카카오 카페 정보 테이블
 CREATE TABLE IF NOT EXISTS kakao_cafe_info  (
@@ -29,6 +29,20 @@ CREATE TABLE IF NOT EXISTS kakao_cafe_menu  (
     price VARCHAR(50),
     image_url VARCHAR(255),
     dessert_tag VARCHAR(255)
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+STORED AS TEXTFILE;
+
+-- 스크래핑한 카카오 리뷰 테이블
+CREATE TABLE IF NOT EXISTS kakao_platform_review  (
+    cafe_name VARCHAR(100),
+    content STRING,
+    nickname VARCHAR(50),
+    platform STRING DEFAULT 'K',
+    created_date VARCHAR(255),
+    collected_date VARCHAR(255)
 )
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY '\t'
