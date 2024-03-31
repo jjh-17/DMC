5d979978ead53e9c0597a4e4b8234085dd899deb
지준호 <jjho311@naver.com>
2024-03-31 19:54:43 +0900

    [BE] Feat : cafe_info에 refion_code 컬럼 추가. 컬럼 순서 변경

--- hive/sql/integrated_table_init.sql
+++ hive/sql/integrated_table_init.sql
@@ -4,21 +4,22 @@ use A607;
 DROP TABLE IF EXISTS cafe_info;
 CREATE TABLE IF NOT EXISTS cafe_info  (
     cafe_seq BIGINT DEFAULT 0L,
-    name VARCHAR(100),
+    address VARCHAR(255),
+    homepage_url STRING,
+    image_url STRING,
+    is_deleted TINYINT,
+    kakao_name VARCHAR(100),
+    kakao_rating FLOAT,
     latitude DOUBLE,
     longitude DOUBLE,
-    kakao_name VARCHAR(100),
+    name VARCHAR(100),
     naver_name VARCHAR(100),
-    image_url STRING,
-    address VARCHAR(255),
-    tel VARCHAR(50),
-    rating FLOAT,
-    kakao_rating FLOAT,
     opening_hour STRING,
-    url VARCHAR(255),
-    updated_date VARCHAR(255),
-    top_tag TINYINT,
-    is_deleted TINYINT
+    rating FLOAT,
+    region_code VARCHAR(255),
+    tel VARCHAR(50),
+    top_tag VARCHAR(255),
+    updated_date VARCHAR(255)
 )
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY '\t'
@@ -31,10 +32,10 @@ DROP TABLE IF EXISTS cafe_menu;
 CREATE TABLE IF NOT EXISTS cafe_menu  (
     menu_seq BIGINT DEFAULT 0L,
     cafe_seq BIGINT DEFAULT 0L,
-    name VARCHAR(100),
-    price VARCHAR(50),
+    dessert_tag VARCHAR(255),
     image_url STRING,
-    dessert_tag VARCHAR(255)
+    name VARCHAR(100),
+    price VARCHAR(50)
 )
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY '\t'
@@ -47,11 +48,12 @@ DROP TABLE IF EXISTS platform_review;
 CREATE TABLE IF NOT EXISTS platform_review  (
     review_seq BIGINT DEFAULT 0L,
     cafe_seq BIGINT DEFAULT 0L,
-    nickname VARCHAR(50),
+    collected_date VARCHAR(15),
     content STRING,
-    platform CHAR(1),
     created_date VARCHAR(15),
-    collected_date VARCHAR(15)
+    nickname VARCHAR(50),
+    platform CHAR(1),
+    tag VARCHAR(255)
 )
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY '\t'
