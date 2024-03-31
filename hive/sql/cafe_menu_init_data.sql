75c243834bc42293f94129b31ce1d270935c69ef
지준호 <jjho311@naver.com>
2024-03-31 11:26:05 +0900

    [BE] Feat : 카페 메뉴 통합 DB 데이터 삽입 구현

--- hive/sql/cafe_menu_init_data.sql
+++ hive/sql/cafe_menu_init_data.sql
@@ -1,40 +1,61 @@
--- hive -f /home/jjh/A607/sql/cafe_menu_init_data.sql
-
 use A607;
 
--- 네이버 메뉴
+
+-- 네이버 정보를 가지지 않은 카페에 대하여 카카오 메뉴 정보 추가
+TRUNCATE TABLE cafe_menu;
 INSERT INTO 
-    cafe_menu(cafe_seq, name, price, image_url, dessert_tag)
+    cafe_menu(
+        cafe_seq, name, price, image_url, dessert_tag
+    )
 SELECT
-    cafe_seq, name, price, image_url, dessert_tag
+    C.cafe_seq,
+    K.name,
+    K.price,
+    K.image_url,
+    K.dessert_tag
+FROM
+    kakao_cafe_menu AS K
+    LEFT JOIN cafe_info AS C ON K.cafe_name = C.kakao_name
+    WHERE C.naver_name IS NULL;
+
+
+-- 네이버 정보를 가진 카페에 대하여 네이버 메뉴 정보 추가
+INSERT INTO 
+    cafe_menu(
+        cafe_seq, name, price, image_url, dessert_tag
+    )
+SELECT
+    C.cafe_seq,
+    N.name,
+    N.price,
+    N.image_url,
+    N.dessert_tag
 FROM
     naver_cafe_menu AS N
-    LEFT JOIN
-    cafe_info AS C
-    ON N.cafe_name = C.naver_name
-
-
-
-
-
-
-SELECT
-    C.cafe_seq, N.cafe_name, C.name, C.naver_name, N.name
-FROM
-    naver_cafe_menu AS N
-    INNER JOIN cafe_info AS C ON N.cafe_name = C.naver_name
+    LEFT JOIN cafe_info AS C ON N.cafe_name = C.naver_name
     WHERE C.naver_name IS NOT NULL;
 
 
--- 카카오 메뉴
+-- cafe_info에 cafe_seq를 Overwrite
+CREATE TABLE new_cafe_menu AS
+SELECT
+    row_number() OVER () - 1 AS menu_seq,
+    cafe_seq, 
+    name, 
+    price, 
+    image_url, 
+    dessert_tag
+FROM cafe_menu;
+
+INSERT OVERWRITE TABLE cafe_menu
+SELECT * FROM new_cafe_menu;
 
 
--- 
+-- overwrite 하기 위한 임시 테이블 제거
+DROP TABLE new_cafe_menu;
 
-
-
-
-SELECT name, COUNT(*)
-FROM CAFE_INFO
-GROUP BY name
-HAVING COUNT(*) > 1;
+-- export csv
+INSERT OVERWRITE LOCAL DIRECTORY '/home/jjh/A607/data/output/cafe_menu/'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t'
+SELECT * FROM cafe_menu;
