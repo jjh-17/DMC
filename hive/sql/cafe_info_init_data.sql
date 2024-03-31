8f6f6cb03c41ec72db60cc70e867d2e9abf530c7
지준호 <jjho311@naver.com>
2024-03-31 19:57:49 +0900

    [BE] Feat : 'NULL'을 NULL로 변경 코드 추가

--- hive/sql/cafe_info_init_data.sql
+++ hive/sql/cafe_info_init_data.sql
@@ -1,18 +1,31 @@
 use A607;
 
 
--- 네이버와 카카오 카페에서 겹치는 것 INSERT
+-- 네이버와 카카오 카페에서 겹치는 것 INSERT(네이버 우선)
 TRUNCATE TABLE cafe_info;
 INSERT INTO
     cafe_info(
-        name, latitude, longitude, kakao_name, naver_name, 
-        image_url, address, tel, rating, kakao_rating, opening_hour, 
-        url, updated_date, top_tag, is_deleted
+        address, homepage_url, image_url, is_deleted,
+        kakao_name, kakao_rating, latitude, longitude, name, naver_name,
+        opening_hour, rating, region_code, tel, top_tag, updated_date
     )
 SELECT
-    T.naver_name AS name, T.latitude, T.longitude, T.kakao_name, T.naver_name,
-    T.image_url, T.address, T.tel, NULL, T.kakao_rating, T.opening_hour,
-    T.url, date_format(current_date(), 'yyyy-MM-dd'), NULL AS top_tag, 0 AS is_deleted
+    T.address, 
+    T.homepage_url, 
+    T.image_url, 
+    0 AS is_deleted,
+    T.kakao_name, 
+    T.kakao_rating, 
+    T.latitude, 
+    T.longitude, 
+    T.naver_name AS name, 
+    T.naver_name,
+    T.opening_hour, 
+    NULL AS rating, 
+    NULL AS region_code, 
+    T.tel, 
+    NULL AS top_tag, 
+    date_format(current_date(), 'yyyy-MM-dd') AS updated_date
 FROM(
     SELECT
         K.cafe_name AS kakao_name, 
@@ -24,7 +37,7 @@ FROM(
         N.address AS address, 
         N.tel AS tel, 
         N.opening_hour AS opening_hour, 
-        N.homepage_url AS url
+        N.homepage_url AS homepage_url
     FROM 
         naver_cafe_info AS N
         INNER JOIN
@@ -39,20 +52,39 @@ FROM(
             OR
             LEVENSHTEIN(K.cafe_name, N.cafe_name)=0
         )
+        WHERE 
+            K.cafe_name IS NOT NULL AND K.cafe_name!='NULL' AND
+            N.cafe_name IS NOT NULL AND N.cafe_name!='NULL' AND
+            N.latitude IS NOT NULL AND N.latitude!=-1 AND
+            N.longitude IS NOT NULL AND N.longitude!=-1 AND
+            N.address IS NOT NULL AND N.address!='NULL'
 ) AS T;
 
 
 -- 통합 카페 정보에 없으면서 네이버 카페에 있는 것 INSERT
 INSERT INTO
     cafe_info(
-        name, latitude, longitude, kakao_name, naver_name, 
-        image_url, address, tel, rating, kakao_rating, opening_hour, 
-        url, updated_date, top_tag, is_deleted
+        address, homepage_url, image_url, is_deleted,
+        kakao_name, kakao_rating, latitude, longitude, name, naver_name,
+        opening_hour, rating, region_code, tel, top_tag, updated_date
     )
 SELECT
-    T.naver_name AS name, T.latitude, T.longitude, NULL, T.naver_name,
-    T.image_url, T.address, T.tel, NULL, NULL, T.opening_hour,
-    T.url, date_format(current_date(), 'yyyy-MM-dd'), NULL, 0
+    T.address, 
+    T.homepage_url, 
+    T.image_url, 
+    0 AS is_deleted,
+    NULL AS kakao_name, 
+    NULL AS kakao_rating, 
+    T.latitude, 
+    T.longitude, 
+    T.naver_name AS name, 
+    T.naver_name,
+    T.opening_hour, 
+    NULL AS rating, 
+    NULL AS region_code, 
+    T.tel, 
+    NULL AS top_tag, 
+    date_format(current_date(), 'yyyy-MM-dd') AS updated_date
 FROM(
     SELECT
         N.cafe_name AS naver_name, 
@@ -62,24 +94,42 @@ FROM(
         N.address AS address, 
         N.tel AS tel, 
         N.opening_hour AS opening_hour, 
-        N.homepage_url AS url
+        N.homepage_url AS homepage_url
     FROM naver_cafe_info AS N
     LEFT JOIN cafe_info AS C ON N.cafe_name = C.naver_name
-    WHERE C.naver_name IS NULL
+    WHERE 
+        C.naver_name IS NULL AND
+        N.cafe_name IS NOT NULL AND N.cafe_name!='NULL' AND
+        N.latitude IS NOT NULL AND N.latitude!=-1 AND
+        N.longitude IS NOT NULL AND N.longitude!=-1 AND
+        N.address IS NOT NULL AND N.address!='NULL'
 ) AS T;
 
 
 -- 통합 카페 정보에 없으면서 카카오 카페에 있는 것 INSERT
 INSERT INTO
     cafe_info(
-        name, latitude, longitude, kakao_name, naver_name, 
-        image_url, address, tel, rating, kakao_rating, opening_hour, 
-        url, updated_date, top_tag, is_deleted
+        address, homepage_url, image_url, is_deleted,
+        kakao_name, kakao_rating, latitude, longitude, name, naver_name,
+        opening_hour, rating, region_code, tel, top_tag, updated_date
     )
 SELECT
-    T.kakao_name AS name, T.latitude, T.longitude, T.kakao_name, NULL,
-    T.image_url, T.address, T.tel, NULL, T.kakao_rating, T.opening_hour,
-    T.url, date_format(current_date(), 'yyyy-MM-dd'), NULL, 0
+    T.address, 
+    T.homepage_url, 
+    T.image_url, 
+    0 AS is_deleted,
+    T.kakao_name,
+    T.kakao_rating,
+    T.latitude, 
+    T.longitude, 
+    T.kakao_name AS name, 
+    NULL AS naver_name,
+    T.opening_hour, 
+    NULL AS rating, 
+    NULL AS region_code, 
+    T.tel, 
+    NULL AS top_tag, 
+    date_format(current_date(), 'yyyy-MM-dd') AS updated_date
 FROM(
     SELECT 
         K.cafe_name AS kakao_name,
@@ -90,32 +140,38 @@ FROM(
         K.address AS address, 
         K.tel AS tel, 
         K.opening_hour AS opening_hour, 
-        K.homepage_url AS url
+        K.homepage_url AS homepage_url
     FROM kakao_cafe_info AS K
     LEFT JOIN cafe_info AS C ON K.cafe_name = C.kakao_name
-    WHERE C.kakao_name IS NULL
+    WHERE 
+        C.kakao_name IS NULL AND
+        K.cafe_name IS NOT NULL AND K.cafe_name!='NULL' AND
+        K.latitude IS NOT NULL AND K.latitude!=-1 AND
+        K.longitude IS NOT NULL AND K.longitude!=-1 AND
+        K.address IS NOT NULL AND K.address!='NULL'
 ) AS T;
 
 
 -- cafe_info에 cafe_seq를 Overwrite
 CREATE TABLE new_cafe_info AS
 SELECT
-    row_number() OVER () - 1 AS cafe_seq,
-    name,
-    latitude,
-    longitude,
-    kakao_name,
-    naver_name,
-    image_url,
-    address,
-    tel,
-    rating,
-    kakao_rating,
-    opening_hour,
-    url,
-    updated_date,
+    row_number() OVER () + 100 AS cafe_seq,
+    address, 
+    CASE WHEN homepage_url = 'NULL' THEN NULL ELSE homepage_url END,
+    CASE WHEN image_url = 'NULL' THEN NULL ELSE image_url END,
+    is_deleted,
+    CASE WHEN kakao_name = 'NULL' THEN NULL ELSE kakao_name END,
+    CASE WHEN kakao_rating = 'NULL' THEN NULL ELSE kakao_rating END,
+    latitude, 
+    longitude, 
+    CASE WHEN name = 'NULL' THEN NULL ELSE name END,
+    CASE WHEN naver_name = 'NULL' THEN NULL ELSE naver_name END,
+    CASE WHEN opening_hour = 'NULL' THEN NULL ELSE opening_hour END,
+    rating, 
+    region_code, 
+    CASE WHEN tel = 'NULL' THEN NULL ELSE tel END,
     top_tag,
-    is_deleted
+    updated_date
 FROM cafe_info;
 
 INSERT OVERWRITE TABLE cafe_info
--- hive/sql/platform_review_init_data.sql
+++ hive/sql/platform_review_init_data.sql
@@ -5,47 +5,63 @@ use A607;
 TRUNCATE TABLE platform_review;
 INSERT INTO
     platform_review(
-        cafe_seq, nickname, content, platform, created_date, collected_date
+        cafe_seq, collected_date, content, 
+        created_date, nickname, platform, tag
     )
 SELECT
     C.cafe_seq,
-    K.nickname,
+    K.collected_date,
     K.content,
-    'K' AS platform,
     K.created_date,
-    K.collected_date
+    K.nickname,
+    'K' AS platform,
+    NULL AS tag
 FROM
     kakao_platform_review AS K
-    LEFT JOIN cafe_info AS C ON K.cafe_name = C.kakao_name;
+    LEFT JOIN cafe_info AS C ON K.cafe_name = C.kakao_name
+    WHERE 
+        C.cafe_seq IS NOT NULL AND
+        K.nickname IS NOT NULL AND K.nickname!='NULL' AND
+        K.content IS NOT NULL AND K.content!='NULL' AND
+        K.created_date IS NOT NULL AND K.created_date!='NULL' AND
+        K.collected_date IS NOT NULL AND K.collected_date!='NULL';
 
 
 -- 네이버 리뷰 추가
 INSERT INTO
     platform_review(
-        cafe_seq, nickname, content, platform, created_date, collected_date
+        cafe_seq, collected_date, content, 
+        created_date, nickname, platform, tag
     )
 SELECT
     C.cafe_seq,
-    N.nickname,
+    N.collected_date,
     N.content,
-    'N' AS platform,
     N.created_date,
-    N.collected_date
+    N.nickname,
+    'N' AS platform,
+    NULL AS tag
 FROM
     naver_platform_review AS N
-    LEFT JOIN cafe_info AS C ON N.cafe_name = C.naver_name;
-
+    LEFT JOIN cafe_info AS C ON N.cafe_name = C.naver_name
+    WHERE 
+        C.cafe_seq IS NOT NULL AND 
+        N.nickname IS NOT NULL AND N.nickname!='NULL' AND
+        N.content IS NOT NULL AND N.content!='NULL' AND
+        N.created_date IS NOT NULL AND N.created_date!='NULL' AND
+        N.collected_date IS NOT NULL AND N.collected_date!='NULL';
 
 -- platform_review에 cafe_seq를 Overwrite
 CREATE TABLE new_platform_review AS
 SELECT
-    row_number() OVER () - 1 AS menu_seq,
+    row_number() OVER () + 100 AS menu_seq,
     cafe_seq, 
-    nickname, 
+    collected_date,
     content, 
-    platform, 
     created_date, 
-    collected_date
+    nickname, 
+    platform, 
+    tag
 FROM platform_review;
 
 INSERT OVERWRITE TABLE platform_review
