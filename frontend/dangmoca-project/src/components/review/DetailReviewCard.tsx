d5bb1d2f6265fb5fe49c5515014db6f421bd33ac
sujin <ansujin0204@naver.com>
2024-04-04 03:24:26 +0900

    [FE] Feat : 리뷰 판단 데이터 반영

--- frontend/dangmoca-project/src/components/review/DetailReviewCard.tsx
+++ frontend/dangmoca-project/src/components/review/DetailReviewCard.tsx
@@ -4,11 +4,14 @@ import FullBean from "../../assets/icons/full-coffee-bean.svg?react";
 import EmptyHeart from "../../assets/icons/empty-heart.svg?react";
 import FullHeart from "../../assets/icons/full-heart.svg?react";
 import { tagMapper } from "../../utils/tag";
-import defaultImg from '/src/assets/icons/profile.svg';
+import defaultImg from "/src/assets/icons/profile.svg";
 import { memberAPI } from "../../api/memberAPI";
 import { useLoginUserStore } from "../../stores/userStore";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import Angry from "../../assets/icons/angry-face.svg?react";
+import Anxious from "../../assets/icons/anxious-face.svg?react";
+import Grinning from "../../assets/icons/grinning-face.svg?react";
 
 const DetailReviewCard = ({ onLikeClick, ...review }: any) => {
   const { setLoginUser } = useLoginUserStore();
@@ -48,9 +51,19 @@ const DetailReviewCard = ({ onLikeClick, ...review }: any) => {
       Swal.fire({
         title: "당모카 회원 정보만 열람 가능합니다!",
         icon: "error",
-      })
+      });
       console.log(error);
     }
+  };
+
+  let FaceIcon = null;
+  if (review.isPositive === true) {
+    FaceIcon = Grinning;
+  } else if (review.isPositive === false) {
+    FaceIcon = Anxious;
+  }
+  if (review.ad) {
+    FaceIcon = Angry;
   }
 
   return (
@@ -64,12 +77,23 @@ const DetailReviewCard = ({ onLikeClick, ...review }: any) => {
           />
         </div>
         <div className="flex flex-col justify-center">
-          <span className="text-xl md:text-2xl mx-3 items-baseline cursor-pointer" onClick={handleNickNameClick}>
+          <span
+            className="text-xl md:text-2xl mx-3 items-baseline cursor-pointer"
+            onClick={handleNickNameClick}
+          >
             {review.nickname}
-            <img src={review.platform == "K" ? "/src/assets/icons/kakao.png" : review.platform == "N" ? "/src/assets/icons/naver.png" : "/src/assets/icons/coffeebean.svg"} className="inline-block w-6 h-6 mx-2" />
+            <img
+              src={
+                review.platform == "K"
+                  ? "/src/assets/icons/kakao.png"
+                  : review.platform == "N"
+                  ? "/src/assets/icons/naver.png"
+                  : "/src/assets/icons/coffeebean.svg"
+              }
+              className="inline-block w-6 h-6 mx-2"
+            />
           </span>
-          <div className="text-lg">{review.title}
-          </div>
+          <div className="text-lg">{review.title}</div>
           <button />
         </div>
       </div>
@@ -87,19 +111,21 @@ const DetailReviewCard = ({ onLikeClick, ...review }: any) => {
           ))}
         </div>
       )}
-      <div className="flex flex-col w-auto h-52 my-4 justify-around border-primary2 border-2 rounded-2xl bg-primary bg-opacity-5 shadow-lg">
+      <div className={`flex flex-col w-auto h-52 my-4 justify-around border-primary2 border-2 rounded-2xl ${FaceIcon === Angry ? 'bg-red-700 bg-opacity-15' : 'bg-primary bg-opacity-5'} shadow-lg`}>
         {/* 리뷰 별점 & 리뷰 날짜 */}
-        {
-          review.rating > 0 && (
-            <div className="flex justify-between items-center px-4 py-2">
-              <div className="flex flex-row space-x-1">
-                {renderBeans(review.rating)}
-              </div>
-              <p className="font-light text-slate-700">{review.createdDate.split("T")[0]}</p>
+        {review.rating > 0 && (
+          <div className="flex justify-between items-center px-4 py-2">
+            <div className="flex flex-row space-x-1">
+              {renderBeans(review.rating)}
             </div>
-
-          )
-        }
+            <div className="flex items-center">
+              {FaceIcon && <FaceIcon className="w-5 h-5 mr-2" />}
+              <p className="font-light text-slate-700">
+                {review.createdDate.split("T")[0]}
+              </p>
+            </div>
+          </div>
+        )}
 
         {/* 리뷰 내용 */}
         <p className="px-5 py-2 ">{review.content}</p>
@@ -110,12 +136,16 @@ const DetailReviewCard = ({ onLikeClick, ...review }: any) => {
             {review.liked ? (
               <>
                 <FullHeart id="svgIcon" className="mr-2" />
-                <span className="font-semibold text-lg text-red-500">{review.likeCount}</span>
+                <span className="font-semibold text-lg text-red-500">
+                  {review.likeCount}
+                </span>
               </>
             ) : (
               <>
                 <EmptyHeart id="svgIcon" className="mr-2" />
-                <span className="font-semibold text-lg text-slate-500">{review.likeCount}</span>
+                <span className="font-semibold text-lg text-slate-500">
+                  {review.likeCount}
+                </span>
               </>
             )}
           </button>
