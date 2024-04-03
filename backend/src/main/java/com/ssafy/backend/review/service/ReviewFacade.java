53cc06305cb9bd2807fd8b4903ed3c10001d5305
dana <kdn1030@naver.com>
2024-04-03 23:34:59 +0900

    [BE] Fix : tag 리턴값 수정

--- backend/src/main/java/com/ssafy/backend/review/service/ReviewFacade.java
+++ backend/src/main/java/com/ssafy/backend/review/service/ReviewFacade.java
@@ -84,7 +84,8 @@ public class ReviewFacade {
         Map<String, Object> analyzeResult = reviewService.analyzeReview(addReviewDto.getContent());
         Boolean isPositive = reviewService.isPositive(analyzeResult);
         Boolean isAd = false;
-        if (addReviewDto.getRating() >= 5.0) {
+        if (addReviewDto.getRating() == 5) {
+            System.out.println(addReviewDto.getRating());
             isAd = reviewService.isAd(analyzeResult);
         }
         addReviewDto.setPositive(isPositive);
